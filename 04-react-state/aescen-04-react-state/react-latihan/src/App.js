c5881c1a5548f7a0304c43ddb59efc891b6b7cdc
aescen <ash.putra15@gmail.com>
2022-06-29 14:49:09 +0700

    latihan 4

---
+++ 04-react-state/aescen-04-react-state/react-latihan/src/App.js
@@ -0,0 +1,26 @@
+import './App.css'
+import Counter from './components/Counter'
+import Login1 from './components/Login1';
+import Login2 from './components/Login2';
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Counter</h1>
+        <Counter />
+        <br />
+        <h1>Form 1</h1>
+        <Login1 />
+        <br />
+        <h1>Form 2</h1>
+        <Login2 />
+        <br />
+        <Todo />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 04-react-state/aescen-04-react-state/react-latihan/src/components/List.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function List() {
+  return (
+    <div>TodoList</div>
+  )
+}
+
+export default List
